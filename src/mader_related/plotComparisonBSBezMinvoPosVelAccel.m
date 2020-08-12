f24e91d3f375326f3b2cef1da1e7d79d360d1d71
Jesus Tordesillas <jtorde@mit.edu>
2020-08-12 10:53:13 -0400

    minor

--- src/mader_related/plotComparisonBSBezMinvoPosVelAccel.m
+++ src/mader_related/plotComparisonBSBezMinvoPosVelAccel.m
@@ -186,8 +186,8 @@ view2=30;
 
 function plot_arrow_axes(length)
 
-%arrow3d([0 0 0],[0 0 length],20,'cylinder',[0.2,0.1]);
-%arrow3d([0 0 0],[0 length 0],20,'cylinder',[0.2,0.1]);
-%arrow3d([0 0 0],[length 0 0],20,'cylinder',[0.2,0.1]);
+arrow3d([0 0 0],[0 0 length],20,'cylinder',[0.2,0.1]);
+arrow3d([0 0 0],[0 length 0],20,'cylinder',[0.2,0.1]);
+arrow3d([0 0 0],[length 0 0],20,'cylinder',[0.2,0.1]);
 
 end
