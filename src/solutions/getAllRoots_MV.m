a034fb72e9253c6f0f69ceb8ad2f57619f4b5cd7
Jesus Tordesillas <jtorde@mit.edu>
2020-10-08 23:02:17 -0400

    changed name file, and the way the roots are obtained

---
+++ src/solutions/getAllRoots_MV.m
@@ -0,0 +1,33 @@
+% /* ----------------------------------------------------------------------------
+%  * Copyright 2020, Jesus Tordesillas Torres, Aerospace Controls Laboratory
+%  * Massachusetts Institute of Technology
+%  * All Rights Reserved
+%  * Authors: Jesus Tordesillas, et al.
+%  * See LICENSE file for the license information
+%  * -------------------------------------------------------------------------- */
+
+function all_roots=getAllRoots_MV(degree, interv)
+
+% if(degree>7 || degree<=0)
+%     error('not implemented yet!')
+% end
+% 
+% if(degree==1)
+%     tangency_points= []; 
+% else
+% 
+% sol=load(strcat('solutionTangencyPointsDeg',num2str(degree),'.mat'));
+% tangency_points=sol.tangencyPoints;
+% 
+% end
+% 
+% 
+% for i=1:length(tangency_points)
+%     tangency_points(i)=convertNumberFromABtoCD(tangency_points(i),[-1,1],interv);
+% end
+
+    [A,rA]=getA_MV(degree,interv);
+    
+    all_roots=unique(cell2mat(rA));
+
+end
