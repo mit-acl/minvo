a2988cb8cf46d4a4420432f9f4cf05367c510b34
Jesus Tordesillas <jtorde@mit.edu>
2020-10-09 09:03:00 -0400

    changed to the new format of the interval

--- src/conversionMinvo_Bs_Bezier.m
+++ src/conversionMinvo_Bs_Bezier.m
@@ -6,17 +6,20 @@
 %  * See LICENSE file for the license information
 %  * -------------------------------------------------------------------------- */
 
-%% For n=3
 clc; clear;
-%Everything here is for the interval [0,1]
-A_be=getA_Be(3,"01");
-A_bs_seg0=computeMatrixForClampedUniformBSpline(3,0,"01");
-A_bs_seg1=computeMatrixForClampedUniformBSpline(3,1,"01");
-A_bs_rest=computeMatrixForClampedUniformBSpline(3,2,"01");
-A_bs_seg_last2=computeMatrixForClampedUniformBSpline(3,-2,"01");
-A_bs_seg_last=computeMatrixForClampedUniformBSpline(3,-1,"01");
 
-A_mv=getA_MV(3,"01");
+interv=[0,1];
+
+%% For n=3
+
+A_be=getA_Be(3,interv);
+A_bs_seg0=computeMatrixForClampedUniformBSpline(3,0,interv);
+A_bs_seg1=computeMatrixForClampedUniformBSpline(3,1,interv);
+A_bs_rest=computeMatrixForClampedUniformBSpline(3,2,interv);
+A_bs_seg_last2=computeMatrixForClampedUniformBSpline(3,-2,interv);
+A_bs_seg_last=computeMatrixForClampedUniformBSpline(3,-1,interv);
+
+A_mv=getA_MV(3,interv);
 
 Mbs2mv_seg0=A_bs_seg0*inv(A_mv);
 Mbs2mv_seg1=A_bs_seg1*inv(A_mv);
@@ -32,14 +35,13 @@ Mbs2be_seg_last=A_bs_seg_last*inv(A_be);
 
 
 %% For n=2
-clc; clear;
-%Everything here is for the interval [0,1]
-A_be=getA_Be(2,"01");
-A_bs_seg0=computeMatrixForClampedUniformBSpline(2,0,"01");
-A_bs_rest=computeMatrixForClampedUniformBSpline(2,1,"01");
-A_bs_seg_last=computeMatrixForClampedUniformBSpline(2,-1,"01");
 
-A_mv=getA_MV(2,"01");
+A_be=getA_Be(2,interv);
+A_bs_seg0=computeMatrixForClampedUniformBSpline(2,0,interv);
+A_bs_rest=computeMatrixForClampedUniformBSpline(2,1,interv);
+A_bs_seg_last=computeMatrixForClampedUniformBSpline(2,-1,interv);
+
+A_mv=getA_MV(2,interv);
 
 Mbs2mv_seg0=A_bs_seg0*inv(A_mv);
 Mbs2mv_rest=A_bs_rest*inv(A_mv);
@@ -50,9 +52,6 @@ Mbs2be_rest=A_bs_rest*inv(A_be);
 Mbs2be_seg_last=A_bs_seg_last*inv(A_be);
 
 
-
-%Everything here is for the interval [0,1]
-
 % Mbs2mv=A_bs*inv(A_mv);
 % 
 % 
@@ -61,9 +60,9 @@ Mbs2be_seg_last=A_bs_seg_last*inv(A_be);
 %% For n=1
 
 %Everything here is for the interval [0,1]
-A_be=getA_Be(1,"01");
-A_bs=getA_BS(1,"01");
-A_mv=getA_MV(1,"01");
+A_be=getA_Be(1,interv);
+A_bs=getA_BS(1,interv);
+A_mv=getA_MV(1,interv);
 
 Mbs2mv=A_bs*inv(A_mv);
 
