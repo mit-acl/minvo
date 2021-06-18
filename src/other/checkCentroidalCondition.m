aa07f0f748adeadf8d1c29147ea7b6f1aecb6231
Jesus Tordesillas <jtorde@mit.edu>
2021-06-18 10:56:25 -0400

    condition satisfied for higher degrees

--- src/other/checkCentroidalCondition.m
+++ src/other/checkCentroidalCondition.m
@@ -6,36 +6,27 @@
 %  * See LICENSE file for the license information
 %  * -------------------------------------------------------------------------- */
 
-% This file checks whether the centroid of each facet of the simplex is
-% contained in the . 
-% If this condition is true, the vector distance_to_each_centroid should have very small values
-% after running this file
-% Currently the condition is satisfied for degree <=3 (for higher, I think
-% I may be running into numerical errors and tolerances from the solver)
+% This file checks whether the centroid of each facet of the simplex belongs to the convex hull of the curve. 
+% If this condition is true, the values of resnorm printed should be very small 
+% What this file does is: 
+    %1) we compute the convex hull of the curve (a polyhedron)
+    %2) we compute the matrices A, b such that the polyhedron is Ax<=b
+    %3) we compute the distance between that polyhedron and the centroids of the simplex by solving a linear least-squares problem
 
-close all; clc; clear;
+%More info available at https://www.mathworks.com/matlabcentral/answers/108655-convex-hull-higher-dimension
+    
+close all; clc; clear;  addpath(genpath('./../solutions')); addpath(genpath('./../utils'));
 
-addpath(genpath('./solutions'));
-
-degree=3;
-% V=rand(degree,degree+1);
+degree=4; interv=[-1,1];
 
 V=[zeros(degree,1) eye(degree)]; %Standard simplex
-
-
-interv=[-1,1];
-
-P=V*getA_MV(degree,interv)
-
-tan_points_and_extrema=getAllRoots_MV(degree,interv);
-
-%tan_points_and_extrema=[tan_points; min(interv); max(interv)];
+P=V*getA_MV(degree,interv);
 
 syms t real
-T=[];
-for i=0:(degree)
-   T=[t^i ;T];
-end
+T=getT(degree,t);
+
+tt=min(interv):0.05:max(interv);
+samples_curve=double(subs(P*T,t,tt));
 
 
 centroids=[]; %each column will contain a centroid of a facet
@@ -45,50 +36,99 @@ for i=1:size(V,2)
     centroids=[centroids mean(V_tmp,2) ]; %And compute the mean of the rest of the vertexes
 end
 
-distance_to_each_centroid=realmax*ones(1,size(centroids,2));
+%See https://www.mathworks.com/matlabcentral/answers/108655-convex-hull-higher-dimension
 
+[A,b,Aeq,beq]=vert2lcon(samples_curve', 1e-10); %Ax<=b is the inside of the polyhedron
 
-[A rootsA]=getA_MV(degree, interv);
-tan_points_and_extrema=cell2mat(rootsA)';
+options = optimoptions('lsqlin','Algorithm','interior-point','Display','off');
 
-for i=1:size(tan_points_and_extrema,1)
-    disp(i/size(tan_points_and_extrema,1)) %Percentage done
-    for ii=1:size(tan_points_and_extrema,1)
-        a=subs(P*T,t,tan_points_and_extrema(i)); %one extremum of the segment
-        b=subs(P*T,t,tan_points_and_extrema(ii)); %another extremum of the segment
-
-        for j=1:size(centroids,2) %And now compute the distance from that segment to each of the centroids
-            
-           centroid=centroids(:,j);
-           distance= vpa(minimumDistancePointToSegment(a,b,centroid));
-           distance_to_each_centroid(j)=min(distance_to_each_centroid(j), distance);
-           
-%            distance_other=realmax;
-%            for tt=0:0.01:1
-%                point_in_segment=a + tt * (b - a);
-%                distance_other=min(distance_other,norm(point_in_segment-centroid));
-%            end
-%            vpa(distance_other,4)
-%            
-%            distance_to_each_centroid(j)=min(distance_to_each_centroid(j), distance_other);
-           
-        end
-
-    end
-
+for j=1:size(centroids,2)
+%     disp("Starting");
+    centroid=centroids(:,j);
+    [x,resnorm,residual,exitflag,output,lambda]=lsqlin(eye(degree),centroid,A,b,Aeq,beq,[],[],[],options);
+    fprintf('Squared distance to centroid %d = %8.8f\n',j,resnorm)
+%     assert(resnorm<1e-4)
 end
 
-distance_to_each_centroid
+%%
+%%%%%%%%%%%%%%%%%%%%%
+
+% % OLD: 
+% % This file checks whether the centroid of each facet of the simplex is
+% % contained in the . 
+% % If this condition is true, the vector distance_to_each_centroid should have very small values
+% % after running this file
+% % Currently the condition is satisfied for degree <=3 (for higher, I think checking the segments that join two extrema is not enough)
+% 
+% close all; clc; clear; addpath(genpath('./../solutions'));addpath(genpath('./../utils'));
+% 
+% degree=4;
+% V=[zeros(degree,1) eye(degree)]; %Standard simplex
+% interv=[-1,1];
+% P=V*getA_MV(degree,interv);
+% 
+% syms t real
+% T=getT(degree,t);
+% PT=P*T;
+% 
+% 
+% centroids=[]; %each column will contain a centroid of a facet
+% for i=1:size(V,2)
+%     V_tmp=V;
+%     V_tmp(:,i)=[]; %delete that vertex
+%     centroids=[centroids mean(V_tmp,2) ]; %And compute the mean of the rest of the vertexes
+% end
+% 
+% distance_to_each_centroid=realmax*ones(1,size(centroids,2));
+% 
+% 
+% [A rootsA]=getA_MV(degree, interv);
+% tan_points_and_extrema=cell2mat(rootsA)';
+% 
+% for i=1:size(tan_points_and_extrema,1)
+%     disp(i/size(tan_points_and_extrema,1)) %Percentage done
+%     for ii=1:size(tan_points_and_extrema,1)
+%         a=subs(PT,t,tan_points_and_extrema(i)); %one extremum of the segment
+%         b=subs(PT,t,tan_points_and_extrema(ii)); %another extremum of the segment
+% 
+%         for j=1:size(centroids,2) %And now compute the distance from that segment to each of the centroids
+%             
+%            centroid=centroids(:,j);
+%            distance= vpa(minimumDistancePointToSegment(a,b,centroid));
+%            distance_to_each_centroid(j)=min(distance_to_each_centroid(j), distance);
+%            
+% %            distance_other=realmax;
+% %            for tt=0:0.01:1
+% %                point_in_segment=a + tt * (b - a);
+% %                distance_other=min(distance_other,norm(point_in_segment-centroid));
+% %            end
+% %            vpa(distance_other,4)
+% %            
+% %            distance_to_each_centroid(j)=min(distance_to_each_centroid(j), distance_other);
+%            
+%         end
+% 
+%     end
+% 
+% end
+% 
+% distance_to_each_centroid
 
 
 %%%%%%%%%%%%%%%%%%%%%
-
+%%
+% distance_to_each_centroid=realmax*ones(1,size(centroids,2));
 % for extrema=[-1,1]
-%     a=subs(P*T,t,extrema); %one extremum of the segment
+% %     a=subs(P*T,t,extrema); %one extremum of the segment
+%     a=evalPT(P,extrema);
+% 
+% %     a-subs(P*T,t,extrema)
+% %     assert(a-)
+%     
 %     for tt=-1:0.001:1
 %         tt
-%         
-%         b=subs(P*T,t,tt); %another extremum of the segment
+%         b=evalPT(P,tt);
+% %         b=subs(P*T,t,tt); %another extremum of the segment
 %         
 %         for j=1:size(centroids,2) %And now compute the distance from that segment to each of the centroids
 %            distance= vpa(minimumDistancePointToSegment(a,b,centroids(:,j)));
@@ -102,6 +142,12 @@ distance_to_each_centroid
 
 %%%%%%%%%%%%%%%%%%%%%%%
 
+function result=evalPT(P,t)
+    result=[];
+    for i=1:size(P,1)
+        result=[result; polyval(P(i,:),t)];
+    end
+end
 
 
 function d = point_to_line(pt, v1, v2)
