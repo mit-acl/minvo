4b114aa652ef41e0174326fc4443bb60db5484bf
Jesus Tordesillas <jtorde@mit.edu>
2021-10-26 17:47:15 -0400

    added comparison with slefes

--- src/comparison_with_slefes/comparison_with_slefes.m
+++ src/comparison_with_slefes/comparison_with_slefes.m
@@ -14,46 +14,45 @@ all_break_points=min(all_deg+1):max(all_deg+1); %Only used for Slefe
 export_figures=true;
 for deg=all_deg
 
-figure;
+    figure;
 
-deg
+    deg
 
-num_col_plots=numel(all_subdiv);
-%%%%
-j=1;
-for subdiv=all_subdiv
+    num_col_plots=numel(all_subdiv);
+    %%%%
+    j=1;
+    for subdiv=all_subdiv
+        %Generate polynomial passing through random points
+        P=generateRandPol(deg,interv);
 
-%Generate polynomial passing through random points
-P=generateRandPol(deg,interv);
-    
-% [vertices_raw, area_union, vertices_union]=myfunction(P, 'MV',interv, num_int)
-subplot(3,num_col_plots,j);
-[area_union, num_vertices_union, area_hull, num_vertices_hull]=myfunction(P, 'MV', interv, subdiv, 0, true);
-text(0.5,1.3,[' \boldmath{$s=',num2str(subdiv),'$}'],'HorizontalAlignment','center','Units','normalized')
+        % [vertices_raw, area_union, vertices_union]=myfunction(P, 'MV',interv, num_int)
+        subplot(3,num_col_plots,j);
+        [area_union, num_vertices_union, area_hull, num_vertices_hull]=myfunction(P, 'MV', interv, subdiv, 0, true);
+        text(0.5,1.3,[' \boldmath{$s=',num2str(subdiv),'$}'],'HorizontalAlignment','center','Units','normalized')
 
-subplot(3,num_col_plots,num_col_plots+j);
-[area_union, num_vertices_union, area_hull, num_vertices_hull]=myfunction(P, 'Be', interv, subdiv, 0, true);
-subplot(3,num_col_plots,2*num_col_plots+j);
-[area_union, num_vertices_union, area_hull, num_vertices_hull]=myfunction(P, 'Slefe', interv, subdiv, deg+1, true); %Note that we are using deg+1 breakpoints
+        subplot(3,num_col_plots,num_col_plots+j);
+        [area_union, num_vertices_union, area_hull, num_vertices_hull]=myfunction(P, 'Be', interv, subdiv, 0, true);
+        subplot(3,num_col_plots,2*num_col_plots+j);
+        [area_union, num_vertices_union, area_hull, num_vertices_hull]=myfunction(P, 'Slefe', interv, subdiv, deg+1, true); %Note that we are using deg+1 breakpoints
 
-j=j+1
-end
+        j=j+1
+    end
 
 
-subplot(3,num_col_plots,1)
-ht = text(-0.1,0.5,'\textbf{MV}','HorizontalAlignment','center','Units','normalized'); set(ht,'Rotation',90)
+    subplot(3,num_col_plots,1)
+    ht = text(-0.1,0.5,'\textbf{MV}','HorizontalAlignment','center','Units','normalized'); set(ht,'Rotation',90)
 
-subplot(3,num_col_plots,num_col_plots+1)%x_lim_fig(1)-0.1*(x_lim_fig(2)-x_lim_fig(1)),mean(y_lim_fig)
-ht = text(-0.1,0.5,'\textbf{Be}','HorizontalAlignment','center','Units','normalized' ); set(ht,'Rotation',90)
+    subplot(3,num_col_plots,num_col_plots+1)%x_lim_fig(1)-0.1*(x_lim_fig(2)-x_lim_fig(1)),mean(y_lim_fig)
+    ht = text(-0.1,0.5,'\textbf{Be}','HorizontalAlignment','center','Units','normalized' ); set(ht,'Rotation',90)
 
-subplot(3,num_col_plots,2*num_col_plots+1)
-x_lim_fig=xlim; y_lim_fig=ylim; ht = text(-0.1,0.5,['\textbf{','SL$_{\mathbf{',num2str(deg+1),'}}$}'],'HorizontalAlignment','center','Units','normalized' ); set(ht,'Rotation',90)
+    subplot(3,num_col_plots,2*num_col_plots+1)
+    x_lim_fig=xlim; y_lim_fig=ylim; ht = text(-0.1,0.5,['\textbf{','SL$_{\mathbf{',num2str(deg+1),'}}$}'],'HorizontalAlignment','center','Units','normalized' ); set(ht,'Rotation',90)
 
-set(gcf,'Position',[413         608        1638         400])
+    set(gcf,'Position',[413         608        1638         400])
 
-if(export_figures)
-    exportAsPdf(gcf,['comparison_slefes_deg_',num2str(deg)]);
-end
+    if(export_figures)
+        exportAsPdf(gcf,['comparison_slefes_deg_',num2str(deg)]);
+    end
 
 end
 
@@ -61,7 +60,7 @@ end
 
 
 %%  MonteCarlo analysis for many random polynomials
-%   NOTE: THIS SECTION TAKES ~25 MIN to finish. If you simply want to generate the plots, you can load "data_comparison_slefes.mat" in ./other folder
+%   NOTE: THIS SECTION TAKES ~4h to finish. If you simply want to generate the plots, you can load "data_comparison_slefes.mat" 
 
 
 all_MV=[];
@@ -70,19 +69,19 @@ all_Slefe={};
 
 
 counter=1;
-
+tic;
 for deg=all_deg
     for num_int=all_subdiv
         
         counter
         
         tmp_MV=[];    tmp_Be=[];    tmp_Slefe=cell(1,size(all_break_points,2));
-        parfor i=1:1
+        for i=1:100
             i
             P=generateRandPol(deg,interv);
             [a, b, c, d]=myfunction(P, 'MV', interv, num_int, 0, false);     tmp_MV=[tmp_MV;       [a, b, c, d]];
             [a, b, c, d]=myfunction(P, 'Be', interv, num_int, 0, false);     tmp_Be=[tmp_Be;       [a, b, c, d]];
-            for j=1:size(all_break_points,2)
+            for j=1:numel(all_break_points)
                 [a, b, c, d]=myfunction(P, 'Slefe', interv, num_int, all_break_points(j), false);  tmp_Slefe{j}=[tmp_Slefe{j}; [a, b, c, d]];
             end
             
@@ -101,7 +100,7 @@ for deg=all_deg
         all_Be.area_hull(index_num_int,index_deg)=             mean(tmp_Be(:,3));
         all_Be.num_vertices_hull(index_num_int,index_deg)=     mean(tmp_Be(:,4));
         
-        for j=1:size(all_break_points,2)
+        for j=1:numel(all_break_points)
             all_Slefe{j}.area_union(index_num_int,index_deg)=         mean(tmp_Slefe{j}(:,1));
             all_Slefe{j}.num_vertices_union(index_num_int,index_deg)= mean(tmp_Slefe{j}(:,2));
             all_Slefe{j}.area_hull(index_num_int,index_deg)=          mean(tmp_Slefe{j}(:,3));
@@ -111,35 +110,27 @@ for deg=all_deg
         counter=counter+1;
     end
 end
-
+toc
 %%
 
-interv=[-1,1];
-
-all_deg=2:7;%
-all_subdiv=1:6;
-all_break_points=min(all_deg+1):max(all_deg+1); %Only used for Slefe
-
 %%%%%%%%%%%%%%%%%%%%%
-
-all_MV.area_union=zeros(numel(all_subdiv),numel(all_deg));
-all_MV.num_vertices_union=zeros(numel(all_subdiv),numel(all_deg));
-all_MV.area_hull=zeros(numel(all_subdiv),numel(all_deg));
-all_MV.num_vertices_hull=zeros(numel(all_subdiv),numel(all_deg));
-
-all_Be.area_union=zeros(numel(all_subdiv),numel(all_deg));
-all_Be.num_vertices_union=zeros(numel(all_subdiv),numel(all_deg));
-all_Be.area_hull=zeros(numel(all_subdiv),numel(all_deg));
-all_Be.num_vertices_hull=zeros(numel(all_subdiv),numel(all_deg));
-
-all_Slefe={};
-for j=1:size(all_break_points,2)
-    all_Slefe{j}.area_union=         zeros(numel(all_subdiv),numel(all_deg));
-    all_Slefe{j}.num_vertices_union= zeros(numel(all_subdiv),numel(all_deg));
-    all_Slefe{j}.area_hull=          zeros(numel(all_subdiv),numel(all_deg));
-    all_Slefe{j}.num_vertices_hull=  zeros(numel(all_subdiv),numel(all_deg));
-end
-
+% all_MV.area_union=zeros(numel(all_subdiv),numel(all_deg));
+% all_MV.num_vertices_union=zeros(numel(all_subdiv),numel(all_deg));
+% all_MV.area_hull=zeros(numel(all_subdiv),numel(all_deg));
+% all_MV.num_vertices_hull=zeros(numel(all_subdiv),numel(all_deg));
+% 
+% all_Be.area_union=zeros(numel(all_subdiv),numel(all_deg));
+% all_Be.num_vertices_union=zeros(numel(all_subdiv),numel(all_deg));
+% all_Be.area_hull=zeros(numel(all_subdiv),numel(all_deg));
+% all_Be.num_vertices_hull=zeros(numel(all_subdiv),numel(all_deg));
+% 
+% all_Slefe={};
+% for j=1:size(all_break_points,2)
+%     all_Slefe{j}.area_union=         zeros(numel(all_subdiv),numel(all_deg));
+%     all_Slefe{j}.num_vertices_union= zeros(numel(all_subdiv),numel(all_deg));
+%     all_Slefe{j}.area_hull=          zeros(numel(all_subdiv),numel(all_deg));
+%     all_Slefe{j}.num_vertices_hull=  zeros(numel(all_subdiv),numel(all_deg));
+% end
 %%%%%%%%%%%%%%%%%%%%%
 
 
@@ -225,19 +216,21 @@ xticklabels(sprintfc('%d',all_deg)); yticklabels(sprintfc('%d',all_seg)); %ytick
 set(gca, 'YTick', 1:numel(all_seg)); set(gca, 'XTick', 1:numel(all_deg));
 %%%%
 
+font_size=4.6;
+
 %%%Plot rectangles
 hold on;
 for i=1:size(matrix_value,1)
     for j=1:size(matrix_value,2)
         
-        value_string=num2str(formatNumber(matrix_value(i,j)));
+        value_string=num2str(formatNumber(matrix_value(i,j),2));
         if(matrix_value(i,j)>1) %MINVO performs better
             rectangle('Position',[j-0.5,i-0.5,1.0,1.0],'EdgeColor','r','LineWidth',1.3)
 %             plot([j-0.5,j+0.5],[i-0.5,i+0.5])
 %             plot(j,i,'.r')
-            text(j,i,['\textbf{',value_string,'}'],'HorizontalAlignment','center','FontSize',6,'Color','r')
+            text(j,i,['\textbf{',value_string,'}'],'HorizontalAlignment','center','FontSize',font_size,'Color','r')
         else
-            text(j,i,value_string,'HorizontalAlignment','center','FontSize',6,'Color','k')
+            text(j,i,value_string,'HorizontalAlignment','center','FontSize',font_size,'Color','k')
         end
     end
 end
@@ -251,41 +244,43 @@ samples_t=linspace(min(interv),max(interv),num_int_subdiv+1);
 
 if(strcmp(basis,'Be'))
     A=getA_Be(n,interv); inv_A=inv(A);
-%     num_vertices_raw=(n+1)*num_int_subdiv -  (num_int_subdiv-1) ;
     color=[179,141,240]/255;
 
 elseif(strcmp(basis,'MV'))
     A=getA_MV(n,interv); inv_A=inv(A);
-%     num_vertices_raw=(n+1)*num_int_subdiv;
     color=[123,218,104]/255;
     
 elseif(strcmp(basis,'Slefe'))
-    
-%     num_vertices_raw=4*num_of_breakpts;
     color=[255,141,59]/255;    
-    
 else
     error("Not implemented yet")
 end
 
+
+
 poly_union=[];
 
-for i=1:(length(samples_t)-1)
-    a=samples_t(i);     b=samples_t(i+1);
-    P_converted=convertCoeffMatrixFromABtoCD(P,[a,b],interv);
+for i=1:(length(samples_t)-1) %For each one of the subdivisions
+    a=samples_t(i);     b=samples_t(i+1); 
     
     if(strcmp(basis,'Be')||strcmp(basis,'MV'))
+        % Times:
+        %
+        %     ├──────────┬──────────────────┬────────────────┤
+        % min(interv)    a                  b             max(interv) 
+        P_converted=convertCoeffMatrixFromABtoCD(P,[a,b],interv); %This is needed because inv_A in expressed in interv
         V=double(P_converted*inv_A);
         [k,~] = convhull(V');
         poly_segment=polyshape(V(1,k),V(2,k)); %V(:,k) are the vertices in the frontier of the convex hull
-        if(isempty(poly_union))  poly_union=poly_segment;, else poly_union=union(poly_union,poly_segment);, end
+        if(isempty(poly_union))  poly_union=poly_segment; else poly_union=union(poly_union,poly_segment); end
+        
     elseif (strcmp(basis,'Slefe'))
-         breakpoints=computeSlefe(P_converted, num_of_breakpts-1, interv);
+         breakpoints=computeSlefe(P, num_of_breakpts-1, [a b]);           
          for j=1:(size(breakpoints,2)-1)
              V=double([breakpoints{j}.vertices breakpoints{j+1}.vertices]);
              [k,~] = convhull(V');
              poly_segment=polyshape(V(1,k),V(2,k)); %V(:,k) are the vertices in the frontier of the convex hull
-            if(isempty(poly_union))  poly_union=poly_segment;, else poly_union=union(poly_union,poly_segment);, end
+             if(isempty(poly_union))  poly_union=poly_segment; else poly_union=union(poly_union,poly_segment); end
          end
     end
   
@@ -312,7 +307,7 @@ if(do_plot)
     hold on; plot(poly_union,'FaceColor',color)
     plot(vertices_union(:,1),vertices_union(:,2),'.k', 'MarkerSize',15)
     plotCurve(P,interv); %axis off;
-    title(['Ve=[',num2str(num_vertices_union),', ',num2str(num_vertices_hull),'] Ar=[',formatNumber(area_union),', ', formatNumber(area_hull),']'],'FontSize',8.2)
+    title(['Ve=[',num2str(num_vertices_union),', ',num2str(num_vertices_hull),'] Ar=[',formatNumber(area_union,3),', ', formatNumber(area_hull,3),']'],'FontSize',8.2)
     box on;
     set(gca,'xtick',[]); set(gca,'xticklabel',[])
     set(gca,'ytick',[]); set(gca, 'yticklabel',[])
@@ -322,13 +317,13 @@ end
 
 end
 
-function result=formatNumber(number)
+function result=formatNumber(number, digits)
     tmp=num2str(number,2);
     if(contains(tmp,'e'))
         %Number is in engineering format (e.g., 2e+5)
         result=strrep(tmp,'+0',''); %strrep will change 1e+02--> 1e2 (for shorter notation)
     else
-        result=num2str(number,'%.2f');
+        result=num2str(number,['%.',num2str(digits),'f']);
     end
 end
 
